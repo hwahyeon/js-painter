d122b4a925787b885dfc7f3c4538ef1fc9f5a2a9
hwahyeon <hwahyeon@knou.ac.kr>
2022-04-22 16:57:40 +0900

    Add html, css, js

---
+++ app.js
@@ -0,0 +1,34 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillColor
+
+let painting = false;
+
+function stopPainting(){
+    painting = false;
+}
+
+
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+}
+
+function onMouseDown(event) {
+    painting = true;
+}
+
+function onMouseUp(event) {
+    stopPainting()
+}
+
+
+
+
+if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
