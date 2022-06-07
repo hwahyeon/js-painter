e5b80e1adacd558b8e1f223a5d6f7633b950f2cf
hwahyeon <hwahyeon@knou.ac.kr>
2022-06-07 13:50:09 +0900

    Create Brush size function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById('jsRange');
 
 
 canvas.width = 700;
@@ -40,6 +41,12 @@ function handleColorClick(event) {
 }
 
 
+function handleRangeChange(event) {
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
@@ -51,3 +58,9 @@ if (canvas) {
 Array.from(colors).forEach(color =>
     color.addEventListener("click", handleColorClick)
 );
+
+
+if(range){
+    range.addEventListener("input", handleRangeChange)
+}
+
